fa6449e00585ce98bdfe2bc6d8bed8a83c83dca4
Bharath Vedartham <vedabharath12345@gmail.com>
2026-03-30 21:58:24 +0530

    apply bpo params while computing fork digest

--- pkg/p2p/fork.go
+++ pkg/p2p/fork.go
@@ -1,6 +1,8 @@
 package p2p
 
 import (
+	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
 
 	"github.com/attestantio/go-eth2-client/spec/phase0"
@@ -23,6 +25,23 @@ func ComputeForkDigest(forkVersion phase0.Version, genesisValidatorsRoot phase0.
 	copy(digest[:], root[:4])
 
 	return digest, nil
+}
+
+// ApplyBPO XORs the fork digest with hash(bpoEpoch || maxBlobsPerBlock)[:4].
+// This matches Prysm's BPO (Blob Parameters Only) fork digest modification for Fulu+ forks.
+func ApplyBPO(digest [4]byte, bpoEpoch uint64, maxBlobsPerBlock uint64) [4]byte {
+	var hb [16]byte
+	binary.LittleEndian.PutUint64(hb[0:8], bpoEpoch)
+	binary.LittleEndian.PutUint64(hb[8:16], maxBlobsPerBlock)
+
+	bpoHash := sha256.Sum256(hb[:])
+
+	return [4]byte{
+		digest[0] ^ bpoHash[0],
+		digest[1] ^ bpoHash[1],
+		digest[2] ^ bpoHash[2],
+		digest[3] ^ bpoHash[3],
+	}
 }
 
 // BuildTopicName constructs a full gossip topic string in the format:
