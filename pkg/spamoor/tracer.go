70676c0cda00f172865e9dc7b388021539523def
Bharath Vedartham <vedabharath12345@gmail.com>
2026-05-28 13:16:18 +0530

    log msg id

--- pkg/spamoor/tracer.go
+++ pkg/spamoor/tracer.go
@@ -1,6 +1,8 @@
 package spamoor
 
 import (
+	"encoding/hex"
+
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/protocol"
@@ -19,69 +21,69 @@ func newLoggingRawTracer(log logrus.FieldLogger) *loggingRawTracer {
 }
 
 func (t *loggingRawTracer) AddPeer(p peer.ID, proto protocol.ID) {
-	t.log.WithFields(logrus.Fields{"peer": p.String(), "proto": string(proto)}).Debug("BHARATH:AddPeer")
+	t.log.WithFields(logrus.Fields{"peer": p.String(), "proto": string(proto)}).Info("BHARATH:AddPeer")
 }
 
 func (t *loggingRawTracer) RemovePeer(p peer.ID) {
-	t.log.WithField("peer", p.String()).Debug("BHARATH:RemovePeer")
+	t.log.WithField("peer", p.String()).Info("BHARATH:RemovePeer")
 }
 
 func (t *loggingRawTracer) Join(topic string) {
-	t.log.WithField("topic", topic).Debug("BHARATH:Join")
+	t.log.WithField("topic", topic).Info("BHARATH:Join")
 }
 
 func (t *loggingRawTracer) Leave(topic string) {
-	t.log.WithField("topic", topic).Debug("BHARATH:Leave")
+	t.log.WithField("topic", topic).Info("BHARATH:Leave")
 }
 
 func (t *loggingRawTracer) Graft(p peer.ID, topic string) {
-	t.log.WithFields(logrus.Fields{"peer": p.String(), "topic": topic}).Debug("BHARATH:Graft")
+	t.log.WithFields(logrus.Fields{"peer": p.String(), "topic": topic}).Info("BHARATH:Graft")
 }
 
 func (t *loggingRawTracer) Prune(p peer.ID, topic string) {
-	t.log.WithFields(logrus.Fields{"peer": p.String(), "topic": topic}).Debug("BHARATH:Prune")
+	t.log.WithFields(logrus.Fields{"peer": p.String(), "topic": topic}).Info("BHARATH:Prune")
 }
 
 func (t *loggingRawTracer) ValidateMessage(msg *pubsub.Message) {
-	t.log.WithFields(messageFields(msg)).Debug("BHARATH:ValidateMessage")
+	t.log.WithFields(messageFields(msg)).Info("BHARATH:ValidateMessage")
 }
 
 func (t *loggingRawTracer) DeliverMessage(msg *pubsub.Message) {
-	t.log.WithFields(messageFields(msg)).Debug("BHARATH:DeliverMessage")
+	t.log.WithFields(messageFields(msg)).Info("BHARATH:DeliverMessage")
 }
 
 func (t *loggingRawTracer) RejectMessage(msg *pubsub.Message, reason string) {
 	fields := messageFields(msg)
 	fields["reason"] = reason
-	t.log.WithFields(fields).Debug("BHARATH:RejectMessage")
+	t.log.WithFields(fields).Info("BHARATH:RejectMessage")
 }
 
 func (t *loggingRawTracer) DuplicateMessage(msg *pubsub.Message) {
-	t.log.WithFields(messageFields(msg)).Debug("BHARATH:DuplicateMessage")
+	t.log.WithFields(messageFields(msg)).Info("BHARATH:DuplicateMessage")
 }
 
 func (t *loggingRawTracer) ThrottlePeer(p peer.ID) {
-	t.log.WithField("peer", p.String()).Debug("BHARATH:ThrottlePeer")
+	t.log.WithField("peer", p.String()).Info("BHARATH:ThrottlePeer")
 }
 
 func (t *loggingRawTracer) RecvRPC(rpc *pubsub.RPC) {
-	t.log.WithFields(rpcFields(rpc)).Debug("BHARATH:RecvRPC")
+	t.log.WithFields(rpcFields(rpc)).Info("BHARATH:RecvRPC")
 }
 
 func (t *loggingRawTracer) SendRPC(rpc *pubsub.RPC, p peer.ID) {
 	fields := rpcFields(rpc)
 	fields["peer"] = p.String()
-	t.log.WithFields(fields).Debug("BHARATH:SendRPC")
+	t.log.WithFields(fields).Info("BHARATH:SendRPC")
 }
 
 func (t *loggingRawTracer) DropRPC(rpc *pubsub.RPC, p peer.ID) {
 	fields := rpcFields(rpc)
 	fields["peer"] = p.String()
-	t.log.WithFields(fields).Debug("BHARATH:DropRPC")
+	t.log.WithFields(fields).Info("BHARATH:DropRPC")
 }
 
 func (t *loggingRawTracer) UndeliverableMessage(msg *pubsub.Message) {
-	t.log.WithFields(messageFields(msg)).Debug("BHARATH:UndeliverableMessage")
+	t.log.WithFields(messageFields(msg)).Info("BHARATH:UndeliverableMessage")
 }
 
 func messageFields(msg *pubsub.Message) logrus.Fields {
@@ -95,6 +97,7 @@ func messageFields(msg *pubsub.Message) logrus.Fields {
 		fields["topic"] = topic
 	}
 	if msg.ID != "" {
+		fields["msg_id"] = hex.EncodeToString([]byte(msg.ID))
 		fields["msg_id_len"] = len(msg.ID)
 	}
 	return fields
